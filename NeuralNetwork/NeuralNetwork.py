89e10caca723b3cf2a41917d1cd76a09df94ae9a
Moritz <22204103+momower1@users.noreply.github.com>
2019-09-01 12:07:38 +0200

    Added CIFAR10 test code to the python project

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -1,4 +1,27 @@
+import random
 import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
-t = torch.rand(5, 4)
-print(t)
+# Load and normalize CIFAR10 dataset
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Function to show an image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(npimg)
+    plt.show()
+
+# Show some random images with the labels
+for i in range(0, 5):
+    rnd = random.randrange(0, len(trainloader.dataset))
+    print(trainloader.dataset.classes[trainloader.dataset.targets[rnd]])
+    imshow(torch.from_numpy(trainloader.dataset.data[rnd]))
