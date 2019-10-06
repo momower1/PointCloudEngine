0775e594829f055fce810ceb540434d2ee8a51fe
Moritz <22204103+momower1@users.noreply.github.com>
2019-10-06 18:40:25 +0200

    Input is now based on .png renderings

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -5,43 +5,46 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Function to show an image
-def imshow(img):
-    npimg = img.detach().numpy()
-    npimg = np.transpose(npimg, axes=(1, 2, 0))
-    plt.imshow(npimg)
+# Function to show an image from a tensor
+def ShowImage(image):
+    npimage = image.detach().numpy()
+    npimage = np.transpose(npimage, axes=(1, 2, 0))
+    plt.imshow(npimage)
     plt.show()
 
+# Load rendered images from .png files in folders "/data/Testset/GroundTruth/" and "/data/Testset/Input/"
 testset = torchvision.datasets.ImageFolder(root='./data/Testset/', transform=torchvision.transforms.ToTensor())
 testloader = torch.utils.data.DataLoader(testset, batch_size=3, shuffle=False, num_workers=0)
 
+# Show the images by using an iterator
 iterator = iter(testloader)
 
 print('Showing splats')
 splats = iterator.next()
-imshow(splats[0][0])
-imshow(splats[0][1])
-imshow(splats[0][2])
+ShowImage(splats[0][0])
+#ShowImage(splats[0][1])
+#ShowImage(splats[0][2])
 
 print('Showing sparse points')
 sparsePoints = iterator.next()
-imshow(sparsePoints[0][0])
-imshow(sparsePoints[0][1])
-imshow(sparsePoints[0][2])
+ShowImage(sparsePoints[0][0])
+#ShowImage(sparsePoints[0][1])
+#ShowImage(sparsePoints[0][2])
 
-# TESTING: Load neural network
-# Input and Output: 3 Channel Color (RGB), 1 Channel Depth
+# Load and evaluate neural network
 model = torch.jit.load('./data/Pytorch_Jit_Model_Lucy.pt')
 model = model.cuda()
 print('Loaded model')
 
-# Evaluate model with random input
-input = torch.randn(1, 2, 1024, 1024)
+# Evaluate model with input from files
+# Input: 1 Channel Color (R, G or B), 1 Channel Depth
+# Output: 1 Channel Color, 1 Channel Depth, 1 Channel Visibility Mask
+input = sparsePoints[0].narrow(1, 0, 2)
 input = input.cuda()
 output = model(input)
 print('Evaluated model')
 
-# Show output image
+# Show output image from the neural network
 print('Showing output')
 output = output.cpu()
-imshow(output[0])
+ShowImage(output[0])
