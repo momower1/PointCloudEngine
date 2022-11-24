bfac98847830f0e484986e56f598c878853c873c
Moritz <22204103+momower1@users.noreply.github.com>
2022-11-24 17:32:35 +0100

    Also loading MTL file

--- PointCloudEngine/OBJFile.cpp
+++ PointCloudEngine/OBJFile.cpp
@@ -3,6 +3,8 @@
 
 OBJFile::OBJFile(std::wstring filename)
 {
+    std::wstring directory = filename.substr(0, filename.find_last_of(L"\\/")) + L"/";
+
     std::wifstream objFile(filename);
     ERROR_MESSAGE_ON_FAIL(objFile.is_open(), L"Cannot open OBJ file \"" + filename + L"\"!");
 
@@ -13,6 +15,7 @@ OBJFile::OBJFile(std::wstring filename)
     std::vector<Vector2> textureCoordinates;
     std::vector<Vector3> normals;
     std::map<std::wstring, std::vector<OBJTriangle>> triangles;
+    std::map<std::wstring, std::wstring> materialTextures;
     std::wstring materialName = L"";
 
     std::wstring line;
@@ -64,7 +67,42 @@ OBJFile::OBJFile(std::wstring filename)
         }
     }
 
-    std::cout << "DONE" << std::endl;
+    // MTL file is assumed to be in the same directory as the OBJ file
+    if (materialTemplateLibraryFilename.length() > 0)
+    {
+        // Prepend the directory filepath of the OBJ file to the MTL file
+        materialTemplateLibraryFilename = directory + materialTemplateLibraryFilename;
+
+        std::wifstream mtlFile(materialTemplateLibraryFilename);
+        ERROR_MESSAGE_ON_FAIL(mtlFile.is_open(), L"Cannot open MTL file \"" + materialTemplateLibraryFilename + L"\" that is associated with the OBJ file \"" + filename + L"\"!");
+
+        // For now this just considers diffuse textures
+        while (std::getline(mtlFile, line))
+        {
+            std::cout << std::string(line.begin(), line.end()) << std::endl;
+
+            std::vector<std::wstring> values = Utils::SplitString(line, L" ");
+
+            if (values.size() > 0)
+            {
+                std::wstring key = values.front();
+
+                if (key == L"newmtl")
+                {
+                    materialName = values[1];
+                }
+                else if (key == L"map_Kd")
+                {
+                    materialTextures[materialName] = directory + values[1];
+                }
+            }
+        }
+    }
+
+    // TODO
+    // - convert into single vertex buffer and single index buffer
+    // - load the textures into a texture array (but they might have different sizes!?)
+    std::cout << "TODO" << std::endl;
 }
 
 std::vector<Vertex> OBJFile::GetVertices()
