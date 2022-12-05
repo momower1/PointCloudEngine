adcf4324f573b7eb1d9ce075a62994cb847384a9
Moritz <22204103+momower1@users.noreply.github.com>
2022-12-05 14:18:41 +0100

    Loading zip file dataset entry

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -1,9 +1,21 @@
 import numpy
 import matplotlib
 import matplotlib.pyplot as plt
+from zipfile import ZipFile
 
 # Use different matplotlib backend to avoid weird error
 #matplotlib.use('Agg')
+
+def LoadTextureFromBytes(bytes):
+    width = numpy.frombuffer(buffer=textureBytes, dtype='int32', count=1, offset=0).item()
+    height = numpy.frombuffer(buffer=textureBytes, dtype='int32', count=1, offset=4).item()
+    channels = numpy.frombuffer(buffer=textureBytes, dtype='int32', count=1, offset=8).item()
+    elementSizeInBytes = numpy.frombuffer(buffer=textureBytes, dtype='int32', count=1, offset=12).item()
+    texture = numpy.frombuffer(buffer=textureBytes, dtype='float16', count=-1, offset=16)
+    texture = numpy.reshape(texture, (height, width, channels))
+    texture = texture.astype('float32')
+
+    return texture
 
 def NormalizeDepthTexture(texture):
     h, w, c = texture.shape
@@ -24,22 +36,18 @@ def NormalizeDepthTexture(texture):
 
     return textureNormalized
 
-textureFile = open('D:/Downloads/PointCloudEngineDataset/MeshDepth.texture', 'rb')
-textureBytes = textureFile.read()
+archive = ZipFile('D:/Downloads/PointCloudEngineDataset/0.zip', 'r')
 
-width = numpy.frombuffer(buffer=textureBytes, dtype='int32', count=1, offset=0).item()
-height = numpy.frombuffer(buffer=textureBytes, dtype='int32', count=1, offset=4).item()
-channels = numpy.frombuffer(buffer=textureBytes, dtype='int32', count=1, offset=8).item()
-elementSizeInBytes = numpy.frombuffer(buffer=textureBytes, dtype='int32', count=1, offset=12).item()
-texture = numpy.frombuffer(buffer=textureBytes, dtype='float16', count=-1, offset=16)
-texture = numpy.reshape(texture, (height, width, channels))
-texture = texture.astype('float32')
+for filename in archive.namelist():
+    print(filename)
+    textureBytes = archive.read(filename)
+    texture = LoadTextureFromBytes(textureBytes)
 
-#texture = numpy.transpose(texture, axes=(0, 1, 2))
+    #texture = numpy.transpose(texture, axes=(0, 1, 2))
 
-texture = NormalizeDepthTexture(texture)
+    texture = NormalizeDepthTexture(texture)
 
-plt.imshow(texture)
-plt.show()
+    plt.imshow(texture)
+    plt.show()
 
 exit(0)
