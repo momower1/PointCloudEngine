e7d8365a4eec6325e1a0a1c35a0c62c0d36ad0b5
Moritz <22204103+momower1@users.noreply.github.com>
2022-12-15 15:59:39 +0100

    Implemented a single model that also estimates occlusion

--- NeuralNetwork/Training.py
+++ NeuralNetwork/Training.py
@@ -25,20 +25,17 @@ schedulerDecaySkip = 100000
 batchCount = dataset.trainingSequenceCount // batchSize
 
 # Create model, optimizer and scheduler
-modelOcclusion = PullPushModel(5, 1, 16).to(device)
-optimizerOcclusion = torch.optim.Adam(modelOcclusion.parameters(), lr=learningRate, betas=(0.9, 0.999))
-schedulerOcclusion = torch.optim.lr_scheduler.ExponentialLR(optimizerOcclusion, gamma=schedulerDecayRate, verbose=False)
-
-model = PullPushModel(8, 7, 16).to(device)
+model = PullPushModel(8, 8, 16).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learningRate, betas=(0.9, 0.999))
 scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=schedulerDecayRate, verbose=False)
 
+factorOcclusion = 1.0
 factorDepth = 1.0
-factorColor = 3.0
+factorColor = 1.0
 factorNormal = 1.0
 
 # Use this directory for the visualization of loss graphs in the Tensorboard at http://localhost:6006/
-checkpointDirectory += 'Sequence with Flow/'
+checkpointDirectory += 'Sequence with Flow Inpainting Only/'
 summaryWriter = SummaryWriter(log_dir=checkpointDirectory)
 
 # Try to load the last checkpoint and continue training from there
@@ -48,9 +45,6 @@ if os.path.exists(checkpointDirectory + checkpointNameStart + checkpointNameEnd)
 
     epoch = checkpoint['Epoch']
     batchIndexStart = checkpoint['BatchIndexStart']
-    modelOcclusion.load_state_dict(checkpoint['ModelOcclusion'])
-    optimizerOcclusion.load_state_dict(checkpoint['OptimizerOcclusion'])
-    schedulerOcclusion.load_state_dict(checkpoint['SchedulerOcclusion'])
     model.load_state_dict(checkpoint['Model'])
     optimizer.load_state_dict(checkpoint['Optimizer'])
     scheduler.load_state_dict(checkpoint['Scheduler'])
@@ -115,58 +109,83 @@ while True:
                 preloadThreads[threadIndex] = threading.Thread(target=preload_thread_load_next, args=(batchNextSequence, threadIndex, sequenceIndex))
                 preloadThreads[threadIndex].start()
 
-        # TODO: Actually use whole sequence
-        tensors = {}
+        # Generate the output frames either in chronological or in reverse order (starting with the last frame, used to improve temporal stability)
+        frameGenerationOrder = range(dataset.sequenceFrameCount) if (iteration % 2 == 0) else range(dataset.sequenceFrameCount -1, -1, -1)
 
-        for renderMode in dataset.renderModes:
-            tensors[renderMode] = sequence[renderMode][0]
+        # Create the inputs/outputs/targets for each frame in the sequence
+        inputs = []
+        outputs = []
+        targets = []
 
-        # Train the occlusion network
-        inputOcclusion = torch.cat([tensors['PointsSparseForeground'], tensors['PointsSparseDepth'], tensors['PointsSparseNormalScreen']], dim=1)
-        targetOcclusion = tensors['PointsSparseOcclusion']
-        outputOcclusion = modelOcclusion(inputOcclusion)
+        for frameIndex in frameGenerationOrder:
+            input = torch.cat([sequence['PointsSparseForeground'][frameIndex], sequence['PointsSparseDepth'][frameIndex], sequence['PointsSparseColor'][frameIndex], sequence['PointsSparseNormalScreen'][frameIndex]], dim=1)
 
-        # There should be no values for non-foreground pixels
-        outputOcclusion = outputOcclusion * tensors['PointsSparseForeground']
+            # Keep the input surface pixels (should fix issue that already "perfect" input does get blurred a lot)
+            output = model(input)
+            #outputOcclusion = output[:, 0:1, :, :]
+            #maskSurface = sequence['PointsSparseForeground'][frameIndex] * (1.0 - outputOcclusion)
+            #output[:, 1:8, :, :] = maskSurface * input[:, 1:8, :, :] + (1.0 - maskSurface) * output[:, 1:8, :, :]
 
-        lossOcclusion = torch.nn.functional.binary_cross_entropy(outputOcclusion, targetOcclusion, reduction='mean')
-        modelOcclusion.zero_grad()
-        lossOcclusion.backward(retain_graph=False)
-        optimizerOcclusion.step()
+            target = torch.cat([sequence['PointsSparseOcclusion'][frameIndex], sequence['MeshDepth'][frameIndex], sequence['MeshColor'][frameIndex], sequence['MeshNormalScreen'][frameIndex]], dim=1)
 
-        # Plot loss
-        summaryWriter.add_scalar('Occlusion/Loss', lossOcclusion, iteration)
+            inputs.append(input)
+            outputs.append(output)
+            targets.append(target)
 
-        # Train the inpainting network
-        input = torch.cat([tensors['PointsSparseForeground'], tensors['PointsSparseDepth'], tensors['PointsSparseColor'], tensors['PointsSparseNormalScreen']], dim=1)
+        inputs = torch.stack(inputs, dim=0)
+        outputs = torch.stack(outputs, dim=0)
+        targets = torch.stack(targets, dim=0)
 
-        outputOcclusionMask = (outputOcclusion.detach() > 0.5).repeat(1, model.inChannels, 1, 1)
-        input[outputOcclusionMask] = 0.0
+        # Accumulate loss terms over triplets in the sequence
+        lossOcclusion = []
+        lossDepth = []
+        lossColor = []
+        lossNormal = []
 
-        target = torch.cat([tensors['MeshDepth'], tensors['MeshColor'], tensors['MeshNormalScreen']], dim=1)
-        output = model(input)
+        for tripletFrameIndex in range(1, dataset.sequenceFrameCount - 1):
+            inputPrevious = inputs[tripletFrameIndex - 1]
+            input = inputs[tripletFrameIndex]
+            inputNext = inputs[tripletFrameIndex + 1]
 
-        # Keep the input surface pixels (should fix issue that already "perfect" input does get blurred a lot)
-        maskSurface = tensors['PointsSparseForeground'] * outputOcclusion.le(0.5).float()
-        output = maskSurface * input[:, 1:8, :, :] + (1.0 - maskSurface) * output
+            outputPrevious = outputs[tripletFrameIndex - 1]
+            output = outputs[tripletFrameIndex]
+            outputNext = outputs[tripletFrameIndex + 1]
 
-        lossDepth = factorDepth * torch.nn.functional.mse_loss(output[:, 0:1, :, :], target[:, 0:1, :, :], reduction='mean')
-        lossColor = factorColor * torch.nn.functional.mse_loss(output[:, 1:4, :, :], target[:, 1:4, :, :], reduction='mean')
-        lossNormal = factorNormal * torch.nn.functional.mse_loss(output[:, 4:7, :, :], target[:, 4:7, :, :], reduction='mean')
+            targetPrevious = targets[tripletFrameIndex - 1]
+            target = targets[tripletFrameIndex]
+            targetNext = targets[tripletFrameIndex + 1]
 
-        loss = torch.stack([lossDepth, lossColor, lossNormal], dim=0).mean()
+            # TODO: Add temporal loss term using warped frames
+            lossOcclusionTriplet = factorOcclusion * torch.nn.functional.binary_cross_entropy(output[:, 0:1, :, :], target[:, 0:1, :, :], reduction='mean')
+            lossDepthTriplet = factorDepth * torch.nn.functional.mse_loss(output[:, 1:2, :, :], target[:, 1:2, :, :], reduction='mean')
+            lossColorTriplet = factorColor * torch.nn.functional.mse_loss(output[:, 2:5, :, :], target[:, 2:5, :, :], reduction='mean')
+            lossNormalTriplet = factorNormal * torch.nn.functional.mse_loss(output[:, 5:8, :, :], target[:, 5:8, :, :], reduction='mean')
+
+            lossOcclusion.append(lossOcclusionTriplet)
+            lossDepth.append(lossDepthTriplet)
+            lossColor.append(lossColorTriplet)
+            lossNormal.append(lossNormalTriplet)
+
+        # Average all loss terms across the sequence and combine into final loss
+        lossOcclusion = torch.stack(lossOcclusion, dim=0).mean()
+        lossDepth = torch.stack(lossDepth, dim=0).mean()
+        lossColor = torch.stack(lossColor, dim=0).mean()
+        lossNormal = torch.stack(lossNormal, dim=0).mean()
+        loss = torch.stack([lossOcclusion, lossDepth, lossColor, lossNormal], dim=0).mean()
+
+        # Update model parameters
         model.zero_grad()
         loss.backward(retain_graph=False)
         optimizer.step()
 
         # Plot loss
-        summaryWriter.add_scalar('Inpainting/Loss', loss, iteration)
-        summaryWriter.add_scalar('Inpainting/Loss Depth', lossDepth, iteration)
-        summaryWriter.add_scalar('Inpainting/Loss Color', lossColor, iteration)
-        summaryWriter.add_scalar('Inpainting/Loss Normal', lossNormal, iteration)
+        summaryWriter.add_scalar('Losses/Loss', loss, iteration)
+        summaryWriter.add_scalar('Losses/Loss Occlusion', lossOcclusion, iteration)
+        summaryWriter.add_scalar('Losses/Loss Depth', lossDepth, iteration)
+        summaryWriter.add_scalar('Losses/Loss Color', lossColor, iteration)
+        summaryWriter.add_scalar('Losses/Loss Normal', lossNormal, iteration)
 
         if iteration % schedulerDecaySkip == (schedulerDecaySkip - 1):
-            schedulerOcclusion.step()
             scheduler.step()
             print('Learning rate: ' + str(scheduler.get_last_lr()[0]))
 
@@ -179,88 +198,86 @@ while True:
             progress = 'Epoch:\t' + str(epoch) + '\t' + str(int(100 * (batchIndex / batchCount))) + '%'
             print(progress)
 
-            inputDepth = tensors['PointsSparseDepth'][snapshotSampleIndex]
-            inputColor = tensors['PointsSparseColor'][snapshotSampleIndex]
-            inputNormal = tensors['PointsSparseNormalScreen'][snapshotSampleIndex]
+            inputForeground = inputs[snapshotFrameIndex, snapshotSampleIndex, 0:1, :, :]
+            inputDepth = inputs[snapshotFrameIndex, snapshotSampleIndex, 1:2, :, :]
+            inputColor = inputs[snapshotFrameIndex, snapshotSampleIndex, 2:5, :, :]
+            inputNormal = inputs[snapshotFrameIndex, snapshotSampleIndex, 5:8, :, :]
 
-            inputSurfaceDepth = input[snapshotSampleIndex, 1:2, :, :]
-            inputSurfaceColor = input[snapshotSampleIndex, 2:5, :, :]
-            inputSurfaceNormal = input[snapshotSampleIndex, 5:8, :, :]
+            outputOcclusion = outputs[snapshotFrameIndex, snapshotSampleIndex, 0:1, :, :]
+            outputDepth = outputs[snapshotFrameIndex, snapshotSampleIndex, 1:2, :, :]
+            outputColor = outputs[snapshotFrameIndex, snapshotSampleIndex, 2:5, :, :]
+            outputNormal = outputs[snapshotFrameIndex, snapshotSampleIndex, 5:8, :, :]
 
-            targetDepth = target[snapshotSampleIndex, 0:1, :, :]
-            targetColor = target[snapshotSampleIndex, 1:4, :, :]
-            targetNormal = target[snapshotSampleIndex, 4:7, :, :]
+            targetOcclusion = targets[snapshotFrameIndex, snapshotSampleIndex, 0:1, :, :]
+            targetDepth = targets[snapshotFrameIndex, snapshotSampleIndex, 1:2, :, :]
+            targetColor = targets[snapshotFrameIndex, snapshotSampleIndex, 2:5, :, :]
+            targetNormal = targets[snapshotFrameIndex, snapshotSampleIndex, 5:8, :, :]
 
-            outputDepth = output[snapshotSampleIndex, 0:1, :, :]
-            outputColor = output[snapshotSampleIndex, 1:4, :, :]
-            outputNormal = output[snapshotSampleIndex, 4:7, :, :]
+            splatsDepth = sequence['SplatsSparseDepth'][snapshotFrameIndex][snapshotSampleIndex]
+            splatsColor = sequence['SplatsSparseColor'][snapshotFrameIndex][snapshotSampleIndex]
+            splatsNormal = sequence['SplatsSparseNormalScreen'][snapshotFrameIndex][snapshotSampleIndex]
 
-            splatsDepth = tensors['SplatsSparseDepth'][snapshotSampleIndex]
-            splatsColor = tensors['SplatsSparseColor'][snapshotSampleIndex]
-            splatsNormal = tensors['SplatsSparseNormalScreen'][snapshotSampleIndex]
+            pullPushDepth = sequence['PullPushDepth'][snapshotFrameIndex][snapshotSampleIndex]
+            pullPushColor = sequence['PullPushColor'][snapshotFrameIndex][snapshotSampleIndex]
+            pullPushNormal = sequence['PullPushNormalScreen'][snapshotFrameIndex][snapshotSampleIndex]
 
-            pullPushDepth = tensors['PullPushDepth'][snapshotSampleIndex]
-            pullPushColor = tensors['PullPushColor'][snapshotSampleIndex]
-            pullPushNormal = tensors['PullPushNormalScreen'][snapshotSampleIndex]
-
-            fig = plt.figure(figsize=(3 * inputDepth.size(2), 6 * inputDepth.size(1)), dpi=1)
-            fig.add_subplot(6, 3, 1).title#.set_text('Input Color')
+            fig = plt.figure(figsize=(4 * inputDepth.size(2), 5 * inputDepth.size(1)), dpi=1)
+            fig.add_subplot(5, 4, 1).title#.set_text('Input Foreground')
+            plt.imshow(TensorToImage(inputForeground))
+            plt.axis('off')
+            fig.add_subplot(5, 4, 2).title#.set_text('Input Color')
             plt.imshow(TensorToImage(inputColor))
             plt.axis('off')
-            fig.add_subplot(6, 3, 2).title#.set_text('Input Depth')
+            fig.add_subplot(5, 4, 3).title#.set_text('Input Depth')
             plt.imshow(TensorToImage(inputDepth))
             plt.axis('off')
-            fig.add_subplot(6, 3, 3).title#.set_text('Input Normal')
+            fig.add_subplot(5, 4, 4).title#.set_text('Input Normal')
             plt.imshow(TensorToImage(inputNormal))
             plt.axis('off')
 
-            fig.add_subplot(6, 3, 4).title#.set_text('Input Color')
-            plt.imshow(TensorToImage(inputSurfaceColor))
+            fig.add_subplot(5, 4, 5).title#.set_text('Output Occlusion')
+            plt.imshow(TensorToImage(outputOcclusion))
             plt.axis('off')
-            fig.add_subplot(6, 3, 5).title#.set_text('Input Depth')
-            plt.imshow(TensorToImage(inputSurfaceDepth))
-            plt.axis('off')
-            fig.add_subplot(6, 3, 6).title#.set_text('Input Normal')
-            plt.imshow(TensorToImage(inputSurfaceNormal))
-            plt.axis('off')
-
-            fig.add_subplot(6, 3, 7).title#.set_text('Output Color')
+            fig.add_subplot(5, 4, 6).title#.set_text('Output Color')
             plt.imshow(TensorToImage(outputColor))
             plt.axis('off')
-            fig.add_subplot(6, 3, 8).title#.set_text('Output Depth')
+            fig.add_subplot(5, 4, 7).title#.set_text('Output Depth')
             plt.imshow(TensorToImage(outputDepth))
             plt.axis('off')
-            fig.add_subplot(6, 3, 9).title#.set_text('Output Normal')
+            fig.add_subplot(5, 4, 8).title#.set_text('Output Normal')
             plt.imshow(TensorToImage(outputNormal))
             plt.axis('off')
 
-            fig.add_subplot(6, 3, 10).title#.set_text('Target Color')
+            fig.add_subplot(5, 4, 9).title#.set_text('Target Occlusion')
+            plt.imshow(TensorToImage(targetOcclusion))
+            plt.axis('off')
+            fig.add_subplot(5, 4, 10).title#.set_text('Target Color')
             plt.imshow(TensorToImage(targetColor))
             plt.axis('off')
-            fig.add_subplot(6, 3, 11).title#.set_text('Target Depth')
+            fig.add_subplot(5, 4, 11).title#.set_text('Target Depth')
             plt.imshow(TensorToImage(targetDepth))
             plt.axis('off')
-            fig.add_subplot(6, 3, 12).title#.set_text('Target Normal')
+            fig.add_subplot(5, 4, 12).title#.set_text('Target Normal')
             plt.imshow(TensorToImage(targetNormal))
             plt.axis('off')
 
-            fig.add_subplot(6, 3, 13).title#.set_text('Splats Color')
+            fig.add_subplot(5, 4, 14).title#.set_text('Splats Color')
             plt.imshow(TensorToImage(splatsColor))
             plt.axis('off')
-            fig.add_subplot(6, 3, 14).title#.set_text('Splats Depth')
+            fig.add_subplot(5, 4, 15).title#.set_text('Splats Depth')
             plt.imshow(TensorToImage(splatsDepth))
             plt.axis('off')
-            fig.add_subplot(6, 3, 15).title#.set_text('Splats Normal')
+            fig.add_subplot(5, 4, 16).title#.set_text('Splats Normal')
             plt.imshow(TensorToImage(splatsNormal))
             plt.axis('off')
 
-            fig.add_subplot(6, 3, 16).title#.set_text('Pull Push Color')
+            fig.add_subplot(5, 4, 18).title#.set_text('Pull Push Color')
             plt.imshow(TensorToImage(pullPushColor))
             plt.axis('off')
-            fig.add_subplot(6, 3, 17).title#.set_text('Pull Push Depth')
+            fig.add_subplot(5, 4, 19).title#.set_text('Pull Push Depth')
             plt.imshow(TensorToImage(pullPushDepth))
             plt.axis('off')
-            fig.add_subplot(6, 3, 18).title#.set_text('Pull Push Normal')
+            fig.add_subplot(5, 4, 20).title#.set_text('Pull Push Normal')
             plt.imshow(TensorToImage(pullPushNormal))
             plt.axis('off')
 
@@ -268,44 +285,6 @@ while True:
             plt.margins(0, 0)
 
             summaryWriter.add_figure('Snapshots/Epoch' + str(epoch), plt.gcf(), iteration)
-
-            inputDepth = inputOcclusion[snapshotSampleIndex, 1:2, :, :]
-            inputNormal = inputOcclusion[snapshotSampleIndex, 2:5, :, :]
-
-            targetOcclusion = targetOcclusion[snapshotSampleIndex, :, :, :]
-            targetSurface = torch.clone(tensors['PointsSparseColor'][snapshotSampleIndex])
-            targetSurface[(targetOcclusion > 0.5).repeat(3, 1, 1)] = 0.0
-
-            outputOcclusion = outputOcclusion[snapshotSampleIndex, :, :, :]
-            outputSurface = torch.clone(tensors['PointsSparseColor'][snapshotSampleIndex])
-            outputSurface[(outputOcclusion > 0.5).repeat(3, 1, 1)] = 0.0
-
-            fig = plt.figure(figsize=(2 * inputDepth.size(2), 3 * inputDepth.size(1)), dpi=1)
-            fig.add_subplot(3, 2, 1).title#.set_text('Input Depth')
-            plt.imshow(TensorToImage(inputDepth))
-            plt.axis('off')
-            fig.add_subplot(3, 2, 2).title#.set_text('Input Normal')
-            plt.imshow(TensorToImage(inputNormal))
-            plt.axis('off')
-
-            fig.add_subplot(3, 2, 3).title#.set_text('Output Occlusion')
-            plt.imshow(TensorToImage(outputOcclusion))
-            plt.axis('off')
-            fig.add_subplot(3, 2, 4).title#.set_text('Output Surface')
-            plt.imshow(TensorToImage(outputSurface))
-            plt.axis('off')
-
-            fig.add_subplot(3, 2, 5).title#.set_text('Target Occlusion')
-            plt.imshow(TensorToImage(targetOcclusion))
-            plt.axis('off')
-            fig.add_subplot(3, 2, 6).title#.set_text('Target Surface')
-            plt.imshow(TensorToImage(targetSurface))
-            plt.axis('off')
-
-            plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-            plt.margins(0, 0)
-
-            summaryWriter.add_figure('SnapshotsOcclusion/Epoch' + str(epoch), plt.gcf(), iteration)
 
             framePrevious = sequence['MeshColor'][snapshotFrameIndex - 1][snapshotSampleIndex]
             frameCurrent = sequence['MeshColor'][snapshotFrameIndex][snapshotSampleIndex]
@@ -366,9 +345,6 @@ while True:
             checkpoint = {
                 'Epoch' : epoch,
                 'BatchIndexStart' : batchIndex + 1,
-                'ModelOcclusion' : modelOcclusion.state_dict(),
-                'OptimizerOcclusion' : optimizerOcclusion.state_dict(),
-                'SchedulerOcclusion' : schedulerOcclusion.state_dict(),
                 'Model' : model.state_dict(),
                 'Optimizer' : optimizer.state_dict(),
                 'Scheduler' : scheduler.state_dict()
