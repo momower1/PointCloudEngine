4fe51abcb8e9af7feefedb274b2b715740cbff14
Moritz <22204103+momower1@users.noreply.github.com>
2022-12-23 17:29:39 +0100

    CUDA, Pytorch, DirectX interop is working

--- PointCloudEngine/PointCloudEngine.cpp
+++ PointCloudEngine/PointCloudEngine.cpp
@@ -851,6 +851,99 @@ void DrawScene()
     // Draw scene
 	scene->Draw();
 
+	// TODO: Testing CUDA and D3D11 interoperability
+	{
+		//LoadLibrary(L"c10_cuda.dll");
+		LoadLibrary(L"torch_cuda.dll");
+		//LoadLibrary(L"torch_cuda_cpp.dll");
+		//LoadLibrary(L"torch_cuda_cu.dll");
+
+		WARNING_MESSAGE_ON_FAIL(torch::cuda::is_available(), L"CUDA not available!");
+
+		CUresult cudaResult;
+		cudaResult = cuInit(0);
+
+		UINT cudaDeviceCount;
+		CUdevice cudaDevice;
+		cudaResult = cuD3D11GetDevices(&cudaDeviceCount, &cudaDevice, 1, d3d11Device, CUd3d11DeviceList::CU_D3D11_DEVICE_LIST_ALL);
+
+		CUcontext cudaContext;
+		//CUdevice cudaDevice;
+		//cudaResult = cuD3D11CtxCreate(&cudaContext, &cudaDevice, CU_CTX_SCHED_AUTO, d3d11Device);
+
+		cudaResult = cuCtxCreate(&cudaContext, CU_CTX_SCHED_AUTO, cudaDevice);
+
+		CUgraphicsResource cudaResource;
+		cudaResult = cuGraphicsD3D11RegisterResource(&cudaResource, backBufferTexture, CU_GRAPHICS_REGISTER_FLAGS_NONE);
+
+		// Possibly the mapping is not even necessary, depending on how a tensor is created in pytorch
+		CUstream cudaStream;
+		cudaResult = cuStreamCreate(&cudaStream, CU_STREAM_DEFAULT);
+		cudaResult = cuGraphicsMapResources(1, &cudaResource, cudaStream);
+
+		CUarray cudaArray;
+		cudaResult = cuGraphicsSubResourceGetMappedArray(&cudaArray, cudaResource, 0, 0);
+
+		CUDA_ARRAY_DESCRIPTOR cudaArrayDescriptor;
+		cudaResult = cuArrayGetDescriptor(&cudaArrayDescriptor, cudaArray);
+
+		long long elementSizeInBytes = 2;
+		long long c = cudaArrayDescriptor.NumChannels;
+		long long h = cudaArrayDescriptor.Height;
+		long long w = cudaArrayDescriptor.Width;
+
+		// Need to allocate and copy to a cuda device pointer that can be used in pytorch
+		CUdeviceptr cudaData;
+		cudaResult = cuMemAlloc(&cudaData, elementSizeInBytes * c * h * w);
+
+		CUDA_MEMCPY2D memCopy2D;
+		ZeroMemory(&memCopy2D, sizeof(memCopy2D));
+		memCopy2D.srcMemoryType = CU_MEMORYTYPE_ARRAY;
+		memCopy2D.dstMemoryType = CU_MEMORYTYPE_DEVICE;
+		memCopy2D.dstDevice = cudaData;
+		memCopy2D.srcArray = cudaArray;
+		memCopy2D.dstPitch = elementSizeInBytes * c * w;
+		memCopy2D.srcXInBytes = 0;
+		memCopy2D.srcY = 0;
+		memCopy2D.dstXInBytes = 0;
+		memCopy2D.dstY = 0;
+		memCopy2D.WidthInBytes = elementSizeInBytes * c * w;
+		memCopy2D.Height = h;
+
+		cudaResult = cuMemcpy2D(&memCopy2D);
+
+		// TODO: Maybe need to copy to some pointer (that can be used in pytorch) with cuMemcpy2D
+		at::Tensor tensor = torch::from_blob((void*)cudaData, { 1, h, w, c }, c10::TensorOptions().device(torch::kCUDA).dtype(torch::kF16).layout(torch::kStrided));
+
+		// Perform operations on the tensor
+		tensor.index_put_({ at::indexing::Slice(), at::indexing::Slice(), at::indexing::Slice(0, w / 2), at::indexing::Slice() }, 0.0f);
+		//tensor.index_put_({ at::indexing::Slice(), at::indexing::Slice(), at::indexing::Slice(w / 2, w), at::indexing::Slice() }, 1.0f);
+
+		// Copy back into CUDA
+		ZeroMemory(&memCopy2D, sizeof(memCopy2D));
+		memCopy2D.srcMemoryType = CU_MEMORYTYPE_DEVICE;
+		memCopy2D.dstMemoryType = CU_MEMORYTYPE_ARRAY;
+		memCopy2D.dstArray = cudaArray;
+		memCopy2D.srcDevice = (CUdeviceptr)tensor.data_ptr();
+		memCopy2D.dstPitch = elementSizeInBytes * c * w;
+		memCopy2D.srcXInBytes = 0;
+		memCopy2D.srcY = 0;
+		memCopy2D.dstXInBytes = 0;
+		memCopy2D.dstY = 0;
+		memCopy2D.WidthInBytes = elementSizeInBytes * c * w;
+		memCopy2D.Height = h;
+
+		cudaResult = cuMemcpy2D(&memCopy2D);
+
+		cudaResult = cuGraphicsUnmapResources(1, &cudaResource, cudaStream);
+
+		cudaResult = cuGraphicsUnregisterResource(cudaResource);
+
+		cudaResult = cuCtxDestroy(cudaContext);
+
+		std::cout << "Done" << std::endl;
+	}
+
 	// Gamma correction is automatically applied in full screen mode, only apply it to the texture after presenting it to the screen (then screenshots will also be gamma corrected)
 	// In window mode the gamma correction has to be done before presenting it to the screen
 	BOOL fullscreen;
