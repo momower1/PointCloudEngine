1a46d7a898d976f410d02fcacf13ee2a504d4082
Moritz <22204103+momower1@users.noreply.github.com>
2023-01-09 10:04:00 +0100

    Moved optical flow helper functions to another file

--- PointCloudEngine/Mesh.hlsl
+++ PointCloudEngine/Mesh.hlsl
@@ -34,47 +34,6 @@ cbuffer MeshRendererConstantBuffer : register(b0)
     // 4 bytes auto padding
 };  // Total: 528 bytes with constant buffer packing rules
 
-float3 HUEtoRGB(in float H)
-{
-    float R = abs(H * 6 - 3) - 1;
-    float G = 2 - abs(H * 6 - 2);
-    float B = 2 - abs(H * 6 - 4);
-    return saturate(float3(R, G, B));
-}
-
-float3 HSVtoRGB(in float3 HSV)
-{
-    float3 RGB = HUEtoRGB(HSV.x);
-    return ((RGB - 1) * HSV.y + 1) * HSV.z;
-}
-
-float4 FlowToColor(in float2 motion)
-{
-    float motionColorScale = 0.05f;
-
-    // Visualize the motion vector by mapping its angle to color and its length to saturation (no motion -> black)
-    // Up           -> Green
-    // Up Right     -> Orange
-    // Right        -> Red
-    // Down Right   -> Magenta
-    // Down         -> Purple
-    // Down Left    -> Blue
-    // Left         -> Turquoise
-    // Up Left      -> Lime
-    float saturation = saturate(motionColorScale * length(motion));
-
-    // Calculate signed angle and map to hue component
-    float2 direction = normalize(motion);
-    float signedAngle = atan2(direction.y, direction.x) - atan2(0.0f, 1.0f);
-    float hue = 0.5f + 0.5f * (signedAngle / 3.141592654f);
-
-    // Convert from HSV to RGB
-    float3 hsv = float3(hue, saturation, 1.0f);
-    float3 color = float3(1.0, 1.0, 1.0) - HSVtoRGB(hsv);
-
-    return float4(color.r, color.g, color.b, 1.0f);
-}
-
 struct VS_INPUT
 {
     uint positionIndex : POSITION_INDEX;
@@ -135,11 +94,9 @@ float4 PS(VS_OUTPUT input) : SV_TARGET
     positionNDC.y *= -1;
 
     float2 previousPixel = (previousPositionNDC.xy + 1.0f) / 2.0f;
-    //previousPixel -= 0.5f / float2(width, height);
     previousPixel *= float2(width, height);
 
     float2 pixel = (positionNDC.xy + 1.0f) / 2.0f;
-    //pixel -= 0.5f / float2(width, height);
     pixel *= float2(width, height);
 
     switch (shadingMode)
@@ -173,13 +130,11 @@ float4 PS(VS_OUTPUT input) : SV_TARGET
         {
             float2 flow = previousPixel - pixel;
             return float4(flow.x, flow.y, 0, 1);
-            //return FlowToColor(flow);
         }
         case SHADING_MODE_OPTICAL_FLOW_BACKWARD:
         {
             float2 flow = pixel - previousPixel;
             return float4(flow.x, flow.y, 0, 1);
-            //return FlowToColor(flow);
         }
     }
 
